670bba8f05a61dba747abdf0ff71113bb63eca85
brtytar <lenopod6@gmail.com>
2025-07-19 17:54:17 -0300

    Montagem de host
    método para adicionar as informações a serem requisitadas no endpoint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import requests
-
 def get_http_data(host: str):
     try:
+        import requests
         
         url = host.replace("\\", "/")
 
@@ -18,15 +17,43 @@ def get_http_data(host: str):
     except requests.RequestException as e:
         return {'erro': str(e)}
 
+
+
+
+def hostendpoint (host):
+    with open ("endpoint.txt") as leitor:
+        # print (host + leitor.read())
+        for linha in leitor:
+            print (host + linha)
+            dados = get_http_data(host + linha)
+            if 'erro' in dados:
+                print("Erro:", dados['erro'])
+            else:
+                print("\n🔸 Headers:\n", dados['headers'])
+                print("\n🔸 Cookies:\n", dados['cookies'])
+                print("\n🔸 Conteúdo (início):\n", dados['content'][:1000])  # Mostra só os primeiros 1000 caracteres
+
+
+
+
+hostendpoint ("https://www.uol.com.br/")       
+
+
 if __name__ == "__main__":
-    endpoint = "ultimas"
-    host = f"https://www.uol.com.br\\{endpoint}"
+    #  endpoint = "ultimas"
+    # host = f"https://www.uol.com.br\\{endpoint}"
+
+    # dados = get_http_data(host)
+
+    # if 'erro' in dados:
+    #     print("Erro:", dados['erro'])
+    # else:
+    #     print("\n🔸 Headers:\n", dados['headers'])
+    #     print("\n🔸 Cookies:\n", dados['cookies'])
+    #     print("\n🔸 Conteúdo (início):\n", dados['content'][:1000])  # Mostra só os primeiros 1000 caracteres
+
+    print (hostendpoint ("https://www.uol.com.br\\"))
 
     dados = get_http_data(host)
 
-    if 'erro' in dados:
-        print("Erro:", dados['erro'])
-    else:
-        print("\n🔸 Headers:\n", dados['headers'])
-        print("\n🔸 Cookies:\n", dados['cookies'])
-        print("\n🔸 Conteúdo (início):\n", dados['content'][:1000])  # Mostra só os primeiros 1000 caracteres
+    
